61c8a88bd314e7e15d29424806160c76799fdbcb
alejandraRamos <alejandra0907ramos@gmail.com>
2020-05-14 21:42:26 -0500

    Ready database mysql

--- server.js
+++ server.js
@@ -1,15 +1,125 @@
 // Get our dependencies
 var express = require('express');
 var app = express();
-//var mysql = require("mysql");
-//var connection = mysql.createConnection({
-//  host     : process.env.DB_HOST || 'mysql-test.cxrpknmq0hfi.us-west-2.rds.amazonaws.com',
-//  user     : process.env.DB_USER || 'applicationuser',
-//  password : process.env.DB_PASS || 'applicationuser',
-//  database : process.env.DB_NAME || 'movie_db'
-//});
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host     : process.env.DB_HOST || 'db-instance-brz.cw49gtamvwvu.us-west-2.rds.amazonaws.com',
+  user     : process.env.DB_USER || 'automatizacionbrz',
+  password : process.env.DB_PASS || 'password',
+  database : process.env.DB_NAME || 'dbbrz',
+  port     : process.env.DB_PORT || '3306'
+});
 
-//connection.connect();
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+  //Table to buy a car
+  var buycars = "CREATE TABLE buycars (title VARCHAR(255), year VARCHAR(255), score VARCHAR(255), price VARCHAR(255), description VARCHAR(255))";
+  connection.query(buycars, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+
+  var insertbuyacar = "INSERT INTO buycars (title, year, score, price, description) VALUES ?";
+  var values = [
+    ['Xerato', '2020', '8', '80.000.000', 'Modern Car 1.6CC'],
+    ['Pikanto', '2020', '6', '40.000.000', 'Modern Car 1.0CC'],
+    ['Rio Zedan', '2016', '9', '30.000.000', 'Modern Car 2.0CC'],
+    ['Zoluto', '2016', '9', '20.000.000', 'Modern Car 3.0CC'],
+    ['Stringer', '2015', '7', '10.000.000', 'Modern Car 3.0CC'],
+    ['Ant-Man', '2015', '8', '70.000.000', 'Modern Car 1.0CC'],
+    ['Rio Jatchbash', '2014', '10', '50.000.000', 'Modern Car 1.0CC']
+  ];
+  con.query(insertbuyacar, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+  });
+
+  //Table to show cars
+  var vehicles = "CREATE TABLE vehicles (name VARCHAR(255), description VARCHAR(255), avatar VARCHAR(1000))";
+  connection.query(vehicles, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+
+  var insertvehicles = "INSERT INTO vehicles (title, description, avatar) VALUES ?";
+  var valuesvehicles = [
+    ['Xerato', 'Modern Car 1.6CC', 'https://www.kia.com/content/dam/kwcms/co/es/images/shoppingtool/Cerato-Showroom.png'],
+    ['Pikanto', 'Modern Car 1.0CC', 'https://www.kia.com/content/dam/kwcms/co/es/images/showroom/PicantoNew/kia-co-picanto-ja.png'],
+    ['Rio Zedan', 'Modern Car 2.0CC', 'https://www.kia.com/content/dam/kwcms/gt/en/images/vehicles/gnb/kia_rio_sc_4dr_17my.png'],
+    ['Zoluto', 'Modern Car 3.0CC', 'https://www.kia.com/content/dam/kwcms/gt/en/images/vehicles/gnb/kia_ab_19my.png'],
+    ['Stringer', 'Modern Car 3.0CC', 'https://www.kia.com/content/dam/kwcms/co/es/images/showroom/stinger/kia-stinger.png'],
+    ['Ant-Man', 'Modern Car 1.0CC', 'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/4/4b/Luis%27_Van_%28Quantum_Tunnel%29.png/revision/latest/scale-to-width-down/310?cb=20181002134025'],
+    ['Rio Jatchbash', 'Modern Car 1.0CC', 'https://www.kia.com/content/dam/kwcms/gt/en/images/vehicles/gnb/kia-rio-sc.png']
+  ];
+  con.query(insertvehicles, [valuesvehicles], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+  });
+
+  //Table to support
+  var support = "CREATE TABLE support (name VARCHAR(255), avatar VARCHAR(1000))";
+  connection.query(support, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+
+  var insertsupport = "INSERT INTO support (name, avatar) VALUES ?";
+  var valuessupport = [
+    ['Mechanics Appointments', 'glyphicon-eye-open'],
+    ['Post-sale service', 'glyphicon-fire'],
+    ['Guarantee', 'glyphicon-time'],
+    ['Online manual', 'glyphicon-record'],
+    ['New AIK', 'glyphicon-heart-empty'],
+    ['safety campaigns', 'glyphicon-globe']
+  ];
+  con.query(insertsupport, [valuessupport], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+  });
+
+  //Table to experience
+  var experience = "CREATE TABLE experience (title VARCHAR(255), description VARCHAR(1000))";
+  connection.query(experience, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+
+  var insertexperience = "INSERT INTO experience (title, description) VALUES ?";
+  var valuesexperience = [
+    ['Misión - AIK','Dar las mejores soluciones en cuanto a servicio postventa y comercializaciòn de vehìculos en el sector de transporte'],
+    ['Visión - AIK', 'Ser el concesionario nùmero uno en ventas y en servicio postventas del paìs'],
+    ['Políticas de Calidad - AIK','Estamos comprometidos con los sistemas de gestión de calidad, medio ambiente, seguridad y salud en el trabajo; reconociendo la importancia del capital humano y asegurando el cumplimiento de los requisitos aplicables'],
+    ['Noticias - AIK','Leonardo Zambrano, Alejandra Ramos, Juan Bolaños']
+  ];
+  con.query(insertexperience, [valuesexperience], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+  });
+
+  //Table to commercial
+  var comerciales = "CREATE TABLE comerciales (titulo VARCHAR(255), imagen VARCHAR(1000), texto VARCHAR(255), autor VARCHAR(255))";
+  connection.query(comerciales, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+
+  var insertcomerciales = "INSERT INTO comerciales (titulo, imagen, texto, autor) VALUES ?";
+  var valuescomerciales = [
+    ['Misión - AIK', 'https://www.kia.com/content/dam/kwcms/gt/en/images/vehicles/gnb/kia-rio-sc.png' , '', 'Leonardo Zambrano'],
+    ['Visión - AIK', 'Vacìo', 'Algo',  'Alejandra Ramos'],
+    ['Políticas de Calidad - AIK', '', '', 'Juan Bolaños']
+  ];
+  con.query(insertcomerciales, [valuescomerciales], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+  });
+
+});
 
 //function getMovies(callback) {    
 //        connection.query("SELECT * FROM movie_db.movies",
@@ -25,71 +135,53 @@ app.get('/', function(req, res){
   res.json(response);
 })
 
-// Implement the movies API endpoint
+// Implement buy cars
 app.get('/buycars', function(req, res){
-  var vehicles = [
-    {title : 'Xerato', release: '2020', score: 8, price: '80.000.000', description : 'Modern Car 1.6CC'},    
-    {title : 'Pikanto', release : '2020', score: 6, price: '40.000.000', description : 'Modern Car 1.0CC'},
-    {title : 'Rio Zedan', release: '2016', score: 9, price: '30.000.000', description : 'Modern Car 2.0CC'},
-    {title : 'Zoluto', release: '2016', score: 9, price: '20.000.000', description : 'Modern Car 3.0CC'},
-    {title : 'Stringer', release : '2015', score: 7, price: '10.000.000', description: 'Modern Car 3.0CC'},
-    {title : 'Ant-Man', release: '2015', score: 8, price: '70.000.000', description : 'Modern Car 1.0CC'},
-    {title : 'Rio Jatchbash', release : '2014', score: 10, price: '50.000.000', description : 'Modern Car 1.0CC'},
-  ]
-
+  connection.query('SELECT * FROM buycars', (err, vehicles) => {
+    if(err)throw err;
+  
   res.json(vehicles);
-})
+
+});    
+});
+
+// Implement vehicles
+app.get('/vehicles', function(req, res){
+  connection.query('SELECT * FROM vehicles', (err, vehicles) => {
+    if(err)throw err;
+  
+  res.json(vehicles);
+
+});    
+});
+
+
+// Implement to support
+app.get('/support', function(req, res){
+  connection.query('SELECT * FROM support', (err, support) => {
+    if(err)throw err;
+  
+  res.json(support);
+});    
+});
+
+// Implement to experience
+app.get('/experience', function(req, res){
+  connection.query('SELECT * FROM experience', (err, experience) => {
+    if(err)throw err;
+  
+  res.json(experience);
+});    
+});
 
 app.get('/comerciales', function(req, res) {
- var comercial = [
-    {titulo: 'Misión - AIK', imagen:'https://www.kia.com/content/dam/kwcms/gt/en/images/vehicles/gnb/kia-rio-sc.png' , texto: '', autor: 'Leonardo Zambrano'},
-    {titulo: 'Visión - AIK', imagen:'Vacìo', texto: 'Algo', autor: 'Alejandra Ramos'},
-    {titulo: 'Políticas de Calidad - AIK', imagen: '', texto: '', autor:'Juan Bolaños'}
-  ]
-//    getMovies(function (err, moviesResult){
- res.json(comercial);
-    });
-//});
+  connection.query('SELECT * FROM experience', (err, comerciales) => {
+    if(err)throw err;
+  
+  res.json(comerciales);
+});    
+});
 
-// Implement the reviewers API endpoint
-app.get('/vehicles', function(req, res){
-  var vehicles = [
-    {name : 'Xerato', description : 'Modern Car 1.6CC', avatar: 'https://www.kia.com/content/dam/kwcms/co/es/images/shoppingtool/Cerato-Showroom.png'},
-    {name: 'Pikanto', description : 'Modern Car 1.6CC', avatar: 'https://www.kia.com/content/dam/kwcms/co/es/images/showroom/PicantoNew/kia-co-picanto-ja.png'},
-    {name: 'Rio zedan', description : 'Modern Car 1.6CC', avatar: 'https://www.kia.com/content/dam/kwcms/gt/en/images/vehicles/gnb/kia_rio_sc_4dr_17my.png'},
-    {name: 'Zoluto', description : 'Modern Car 1.6CC', avatar: 'https://www.kia.com/content/dam/kwcms/gt/en/images/vehicles/gnb/kia_ab_19my.png'},
-    {name: 'Stringer', description: 'Modern Car 1.6CC', avatar: 'https://www.kia.com/content/dam/kwcms/co/es/images/showroom/stinger/kia-stinger.png'},
-    {name: 'Ant-man', description: 'Modern Car 1.6CC', avatar : 'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/4/4b/Luis%27_Van_%28Quantum_Tunnel%29.png/revision/latest/scale-to-width-down/310?cb=20181002134025'},
-    {name: 'Rioc Jatchbash', description : 'Modern Car 1.6CC', avatar :'https://www.kia.com/content/dam/kwcms/gt/en/images/vehicles/gnb/kia-rio-sc.png'}
-  ];
-
-  res.json(vehicles);
-})
-
-// Implement the publications API endpoint
-app.get('/support', function(req, res){
-  var support = [
-    {name : 'Mechanics Appointments', avatar: 'glyphicon-eye-open'},
-    {name : 'Post-sale service', avatar: 'glyphicon-fire'},
-    {name : 'Guarantee', avatar: 'glyphicon-time'},
-    {name : 'Online manual', avatar: 'glyphicon-record'},
-    {name : 'New AIK', avatar: 'glyphicon-heart-empty'},
-    {name : 'safety campaigns', avatar : 'glyphicon-globe'}
-  ];
-
-  res.json(support);
-})
-
-// Implement the pending reviews API endpoint
-app.get('/experience', function(req, res){
-  var experience = [
-    {title : 'Misión - AIK', description: 'Dar las mejores soluciones en cuanto a servicio postventa y comercializaciòn de vehìculos en el sector de transporte'},
-    {title : 'Visión - AIK', description: 'Ser el concesionario nùmero uno en ventas y en servicio postventas del paìs'},
-    {title : 'Políticas de Calidad - AIK', description: 'Estamos comprometidos con los sistemas de gestión de calidad, medio ambiente, seguridad y salud en el trabajo; reconociendo la importancia del capital humano y asegurando el cumplimiento de los requisitos aplicables'},
-    {title : 'Noticias - AIK', description: 'Leonardo Zambrano, Alejandra Ramos, Juan Bolaños'}
-  ]
-  res.json(experience);
-})
 console.log("server listening through port: "+process.env.PORT);
 // Launch our API Server and have it listen on port 3000.
 app.listen(process.env.PORT || 3000);
